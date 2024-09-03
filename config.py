6260722f4aa565cd72bf13e7ae20721f68e8520e
altyyy <arutosato21@icloud.com>
2024-09-03 23:41:16 +0900

    “第１完成形”

--- config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-def get_db_connection():
-    conn = mysql.connector.connect(
-                user='root',
-                password='Arutosato1021',
-                host='localhost',
-                database='scraping'
-            )
-    return conn
